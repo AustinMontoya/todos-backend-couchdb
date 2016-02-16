cff760c23812a35dd6b66cb5eebf2359024bfe3d
grrizzly <austin.r.montoya@gmail.com>
2016-02-16 11:18:21 -0800

    added docker image

--- create-image.sh
+++ create-image.sh
@@ -1,2 +1,10 @@
+
+# See https://github.com/mitchellh/packer/issues/901
+if [[ $(uname) == 'Darwin' ]]; then
+  echo "Setting up workaround for packer's docker builder on OSX"
+  [ ! -d "$HOME/tmp" ] &&  mkdir $HOME/tmp
+  TMPDIR=$HOME/tmp
+fi
+
 tar -czf ./app.tar.gz --exclude=node_modules/* app
-packer build -force templates/app-ubuntu-14.04.3.json
+packer build -force templates/app-$1.json
